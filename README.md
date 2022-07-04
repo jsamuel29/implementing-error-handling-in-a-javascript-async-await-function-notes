cc06bd0969f888efb1cee64d4a4f6f12ae812186
Jonathan Samuel <jonathansamuel83@gmail.com>
2022-07-04 13:48:57 -0500

    notes

---
+++ README.md
@@ -0,0 +1,4 @@
+# Implementing Error Handling In a JavaScript Async / Await Function  notes
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jsamuel1/pen/MWQZJzQ](https://codepen.io/jsamuel1/pen/MWQZJzQ).
+
---
+++ dist/script.js
@@ -0,0 +1,21 @@
+async function queryApis() {
+  try {
+    const postsPromise = fetch('https://api.dailysmarty.com/posts');
+    const posts = await postsPromise.then(res => res.json());
+    console.log(posts);
+  } catch(err) {
+    console.log(err);
+    console.log('There was an error with the DailySmarty API');
+  }
+  
+  try {
+    const reposPromise = fetch('https://api.github.com/users/jordanhudgens/repos');
+    const repos = await reposPromise.then(res => res.json());
+    console.log(repos);
+  } catch(err) {
+    console.log(err);
+    console.log('There was an error with the DailySmarty Github API');
+  }
+}
+
+queryApis();
